81fb4b5cab86af391116e6521b941f7eaaed5b6a
Tomasz Kamiński <kaminski.tomasz.a@gmail.com>
2012-04-21 20:55:09 +0200

    zmieniona wersja springa na 3.1.1-RELEASE. Zmieniony sposób mapowania nazw serwletów w pliku web.xml.
    Projekt w eclipse'ie obsługiwany jest przez wtyczkę m2eclipse-wtp.
    
    Statyczna zawartość projektu w katalogu WEB-INF/resources (przykład:
    static.html)
    
    Signed-off-by: Tomasz Kamiński <kaminski.tomasz.a@gmail.com>

--- 84817_84834/project/source/dentalsurgery/src/main/java/pl/poznan/put/dentalsurgery/service/SimpleEntityServiceImpl.java
+++ 84817_84834/project/source/dentalsurgery/src/main/java/pl/poznan/put/dentalsurgery/service/SimpleEntityServiceImpl.java
@@ -2,10 +2,14 @@ package pl.poznan.put.dentalsurgery.service;
 
 import java.util.Collection;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import pl.poznan.put.dentalsurgery.model.SimpleEntity;
 import pl.poznan.put.dentalsurgery.repository.SimpleEntityDao;
 
 public class SimpleEntityServiceImpl implements SimpleEntityService {
+	protected final Log logger = LogFactory.getLog(getClass());
 
 	private SimpleEntityDao simpleEntityDao;
 
@@ -14,6 +18,7 @@ public class SimpleEntityServiceImpl implements SimpleEntityService {
 	}
 
 	public Collection<SimpleEntity> loadAllEntities() {
+		logger.info("Jestem w SimpleEntityServiceImpl.loadAllEntities!!! :)");
 		return simpleEntityDao.loadAllEntities();
 	}
 
--- 84817_84834/project/source/dentalsurgery/src/main/webapp/WEB-INF/jsp/hello.jsp
+++ 84817_84834/project/source/dentalsurgery/src/main/webapp/WEB-INF/jsp/hello.jsp
@@ -5,9 +5,23 @@
 <title><fmt:message key="title" /></title>
 </head>
 <body>
-	<h1>Witaj w systemie <fmt:message key="title" />!</h1>
+	<h1>
+		Witaj w systemie <fmt:message key="title" />!
+	</h1>
 	<p>
-	Oto napis zapisany w modelu: <c:out value="${someString}"></c:out>
+		Hello!! Oto napis zapisany w modelu:
+		<c:out value="${someString}"></c:out>
+		<br /> Oto lista obiektów z bazy danych:
+	<ul>
+		<c:forEach items="${entityList}" var="entity">
+			<li>Obiekt <c:out value="${entity.id}" />: 
+					<c:out value="${entity.someText}" />,
+					<fmt:formatDate value="${entity.someDate}" pattern="dd-mm-yyyy"/>,
+					<c:out value="${entity.someNumber}" />
+			</li>
+		</c:forEach>
+	</ul>
+
 	</p>
 </body>
 </html>

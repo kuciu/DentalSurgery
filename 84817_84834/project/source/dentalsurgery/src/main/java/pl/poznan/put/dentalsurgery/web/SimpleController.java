56be5109aabba73592db0df9a5be4de9d6606b73
Tomasz Kamiński <kaminski.tomasz.a@gmail.com>
2012-04-22 00:45:20 +0200

    Skoro już jesteśmy przy springu3, to przerobiłem kod na adnotacje :) Zobacz Kuciu jak to zgrabnie wygląda, szczególnie w kwestii kontrolerów. Widok określa się przy definicji kontrolera, wydaje mi się to bardzo wygodne. Transakcje też przerobiłem na definiowane przez adnotacje. Poza tym wszystkie beany są wykrywane przez springa, nic z biznesowych beanów nie jest definiowane w xml'u konfiguracyjnym.
    Signed-off-by: Tomasz Kamiński <kaminski.tomasz.a@gmail.com>

--- 84817_84834/project/source/dentalsurgery/src/main/java/pl/poznan/put/dentalsurgery/web/SimpleController.java
+++ 84817_84834/project/source/dentalsurgery/src/main/java/pl/poznan/put/dentalsurgery/web/SimpleController.java
@@ -1,40 +1,37 @@
 package pl.poznan.put.dentalsurgery.web;
 
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import pl.poznan.put.dentalsurgery.model.SimpleEntity;
 import pl.poznan.put.dentalsurgery.service.SimpleEntityService;
 
-public class SimpleController implements Controller {
+@Controller
+public class SimpleController {
 
 	protected final Log logger = LogFactory.getLog(getClass());
-
 	private SimpleEntityService simpleEntityService;
-	
-	public ModelAndView handleRequest(HttpServletRequest request,
-			HttpServletResponse response) throws Exception {
-		
-		Collection<SimpleEntity> entityList = simpleEntityService.loadAllEntities();
-		
-        Map<String, Object> myModel = new HashMap<String, Object>();
-        myModel.put("entityList", entityList);
-        myModel.put("someString", "napis w obiekcie modelu");
 
-        return new ModelAndView("hello", myModel);
-	}
-
+	@Autowired
 	public void setSimpleEntityService(SimpleEntityService simpleEntityService) {
 		this.simpleEntityService = simpleEntityService;
 	}
-	
+
+	@RequestMapping({"/","/hello"})
+	public String showSomePage(Map<String,Object> model) throws Exception {
+
+		Collection<SimpleEntity> entityList = simpleEntityService
+				.loadAllEntities();
+		model.put("entityList", entityList);
+		model.put("someString", "napis w obiekcie modelu");
+
+		return "hello";
+	}
+
 }

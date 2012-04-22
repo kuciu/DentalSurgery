33ae814b66c8fee9aa70c7a9495af0ee4effae99
Kuciu <kuciu@o2.pl>
2012-04-22 10:34:36 +0200

    test

--- 84817_84834/project/source/dentalsurgery/src/main/java/pl/poznan/put/dentalsurgery/repository/SimpleEntityDaoImpl.java
+++ 84817_84834/project/source/dentalsurgery/src/main/java/pl/poznan/put/dentalsurgery/repository/SimpleEntityDaoImpl.java
@@ -3,19 +3,23 @@ package pl.poznan.put.dentalsurgery.repository;
 import java.util.Collection;
 import pl.poznan.put.dentalsurgery.model.SimpleEntity;
 import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class SimpleEntityDaoImpl implements SimpleEntityDao {
 
-    private SessionFactory sessionFactory;
+	private SessionFactory sessionFactory;
 
-    public void setSessionFactory(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
-    }
-	
+	@Autowired
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+	@SuppressWarnings("unchecked")
 	public Collection<SimpleEntity> loadAllEntities() {
 		return this.sessionFactory.getCurrentSession()
-                .createQuery("from SimpleEntity")
-                .list();
+				.createQuery("from SimpleEntity").list();
 	}
 
 }
